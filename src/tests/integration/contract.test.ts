44aebbe0038266c702f331fd055354c790040998
Oleksandr Pelykh <olexandr13@gmail.com>
2023-06-07 15:16:46 +0300

    add contract test

---
+++ src/tests/integration/contract.test.ts
@@ -0,0 +1,30 @@
+import contract from '../../clients/quicknode';
+import { faker } from '@faker-js/faker';
+import backend from '../../clients/backend';
+
+describe('Backend', () => {
+  let collectionAddress: string;
+  beforeAll(async () => {
+
+  })
+
+  test('created collection should be present in backend events', async () => {
+    collectionAddress = await contract.createCollection(faker.word.noun(), faker.string.alpha({
+      length: 3, casing: 'upper'
+    }), faker.internet.url());
+    await backend.waitForCollectionCreation(collectionAddress);
+  });
+
+  test('created NFT should be present in backend events', async () => {
+    const NFTData = {
+      revepient: '0x476348FdCab51b5F4cC24a38512A3F8F0abdCD8E',
+      tokenId: faker.number.int().toString(),
+    }
+    await contract.mintNFT(NFTData.revepient, NFTData.tokenId);
+    await backend.waitForNFTCreation({
+      collection: collectionAddress,
+      recipient: '0x476348FdCab51b5F4cC24a38512A3F8F0abdCD8E',
+      tokenId: NFTData.tokenId,
+    })
+  });
+});
