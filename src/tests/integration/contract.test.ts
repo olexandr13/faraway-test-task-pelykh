b202861cb47a646ba92a3daba74a46446912251c
Oleksandr Pelykh <olexandr13@gmail.com>
2023-06-08 13:04:54 +0300

    save data to json db inside integration test

--- src/tests/integration/contract.test.ts
+++ src/tests/integration/contract.test.ts
@@ -1,29 +1,41 @@
 import contract from '../../clients/quicknode';
 import { faker } from '@faker-js/faker';
 import backend from '../../clients/backend';
+import JSONdb from 'simple-json-db';
+
+const collectionData = {
+  address: '',
+  name: faker.word.noun(),
+  symbol: faker.string.alpha({
+    length: 3, casing: 'upper'
+  }),
+  uri: faker.internet.url(),
+}
+
+const NFTData = {
+  revepient: '0x476348FdCab51b5F4cC24a38512A3F8F0abdCD8E',
+  tokenId: faker.number.int().toString(),
+}
+
+const db = new JSONdb('./src/tests/testdata.json', { asyncWrite: true, syncOnWrite: true });
 
 describe('Collection tests', () => {
-  let collectionAddress: string;
-  beforeAll(async () => {
-
+  afterAll(async () => {
+    db.set('collection', collectionData);
+    db.set('nft', NFTData);
   })
 
   test('created collection should be present in backend events', async () => {
-    collectionAddress = await contract.createCollection(faker.word.noun(), faker.string.alpha({
-      length: 3, casing: 'upper'
-    }), faker.internet.url());
-    await backend.waitForCollectionCreation(collectionAddress);
+    collectionData.address = await contract.createCollection(collectionData.name, collectionData.symbol, collectionData.uri);
+    await backend.waitForCollectionCreation(collectionData.address);
   });
 
   test('created NFT should be present in backend events', async () => {
-    const NFTData = {
-      revepient: '0x476348FdCab51b5F4cC24a38512A3F8F0abdCD8E',
-      tokenId: faker.number.int().toString(),
-    }
+
     await contract.mintNFT(NFTData.revepient, NFTData.tokenId);
     await backend.waitForNFTCreation({
-      collection: collectionAddress,
-      recipient: '0x476348FdCab51b5F4cC24a38512A3F8F0abdCD8E',
+      collection: collectionData.address,
+      recipient: NFTData.revepient,
       tokenId: NFTData.tokenId,
     })
   });
